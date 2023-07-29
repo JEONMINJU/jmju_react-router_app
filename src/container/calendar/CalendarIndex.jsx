257ed7660e6d483f0257c05d0881f6d387b64ecd
전민주 <junielle.jeon@gmail.com>
2023-07-29 22:10:22 +0900

    feat : react-calendar add

--- src/container/calendar/CalendarIndex.jsx
+++ src/container/calendar/CalendarIndex.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // css import
+import CommonHeader from '../../components/Header/CommonHeader';
+import styled from 'styled-components';
 
 function CalendarIndex() {
-	<>
-		Calendar add
-	</>
+	const [value, onChange] = useState(new Date());
+
+	return (
+		<CalendarIndexSection>
+			<CommonHeader/>
+			<Calendar onChange={onChange} value={value} />
+		</CalendarIndexSection>
+	)
 }
 
 export default CalendarIndex;
+
+const CalendarIndexSection = styled.section`
+	.react-calendar {
+		margin: 30px auto;
+	}
+`
+
