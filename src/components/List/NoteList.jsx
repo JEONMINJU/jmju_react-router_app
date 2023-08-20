d844a0b7b4408e532e02d609d823b32af7a5158c
전민주 <junielle.jeon@gmail.com>
2023-08-20 20:01:33 +0900

    style : swiper button custom

--- src/components/List/NoteList.jsx
+++ src/components/List/NoteList.jsx
@@ -53,17 +53,78 @@ export default function NoteList({ todos, setTodos }) {
 }
 
 const Container = styled.section`
-  padding: 20px;
-  .swiper-slide {
-    li {
-      margin: 30px;
-      border-radius: 6px;
-      border: 1px solid ${theme.color.black};
-    }
-  }
+  // slide custom
+  .swiper {
+    margin-top: 16px;
 
-  .swiper-pagination {
-    bottom: -3px;
+    &-wrapper {
+      margin-top: 34px;
+    }
+
+    &-slide {
+      li {
+        margin: 10px 10px 20px;
+        border-radius: 6px;
+        border: 1px solid ${theme.color.black};
+      }
+    }
+
+    // public 파일 안에 있는 이미지는 서버와 통신이 가능함
+    // import 해서 넣을거아니면 src 폴더가 아닌 public 안에!
+    &-button-next {
+      top: 24px;
+      width: 30px;
+      height: 30px;
+
+      &:before {
+        content: '';
+        display: block;
+        width: 10px; /* 사이즈 */
+        height: 10px; /* 사이즈 */
+        border-top: 2px solid ${theme.color.black}; /* 선 두께 */
+        border-right: 2px solid ${theme.color.black}; /* 선 두께 */
+        transform: rotate(45deg); /* 각도 */
+      }
+
+      &:after {
+        display: none;
+      }
+    }
+
+    &-button-prev {
+      top: 24px;
+      width: 30px;
+      height: 30px;
+      right: 33px;
+      left: auto;
+
+      &:before {
+        content: '';
+        display: block;
+        width: 10px;
+        height: 10px;
+        border-top: 2px solid ${theme.color.black};
+        border-right: 2px solid ${theme.color.black};
+        transform: rotate(225deg);
+      }
+
+      &:after {
+        display: none;
+      }
+    }
+
+    &-pagination {
+      bottom: -2px;
+
+      &-bullet {
+        background: ${theme.color.da};
+        opacity: 1;
+
+        &-active {
+          background: ${theme.color.black};
+        }
+      }
+    }
   }
 `
 // useEffect로 로컬스토리지 todos데이터를 초기셋팅한다
